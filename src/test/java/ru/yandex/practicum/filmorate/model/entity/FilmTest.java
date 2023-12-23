cca3cad07a5b30a9d20f74bbbfbcd64243be729f
Danil Losev <u4iha.danil@yandex.ru>
2023-12-23 21:18:10 +0300

    feat: Добавлены тесты валидации

---
+++ src/test/java/ru/yandex/practicum/filmorate/model/entity/FilmTest.java
@@ -0,0 +1,71 @@
+package ru.yandex.practicum.filmorate.model.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmTest {
+
+    private Film film;
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+
+
+    private Validator validator = factory.getValidator();
+
+    @BeforeEach
+    public void beforeEach() {
+        this.film = new Film(0,"Титаник","Фильм о корабле", LocalDate.of(2023, 11, 2), 124);
+    }
+
+    @Test
+    public void shouldHaveZeroViolations() {
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(0, violations.size());
+    }
+
+    @Test
+    public void shouldHaveOneViolationOnEmptyName() {
+        film.setName("");
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1,  violations.size());
+    }
+
+    @Test
+    public void shouldHaveOneViolationOnLongDescription() {
+        String description = "";
+        for (int i = 0; i < 201; i++) {
+            description += "a";
+        }
+        film.setDescription(description);
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1,  violations.size());
+    }
+
+    @Test
+    public void shouldHaveOneViolationOndNegativeDuration() {
+        film.setDuration(-100);
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1,  violations.size());
+    }
+
+    @Test
+    public void shouldHaveThreeViolationsOnEmptyNameAndLongDescriptionAndNegativeDuration() {
+        film.setName("");
+        String description = "";
+        for (int i = 0; i < 201; i++) {
+            description += "a";
+        }
+        film.setDescription(description);
+        film.setDuration(-100);
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(3,  violations.size());
+    }
+
+
+
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/model/entity/UserTest.java
@@ -0,0 +1,84 @@
+package ru.yandex.practicum.filmorate.model.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private User user;
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+
+
+    private Validator validator = factory.getValidator();
+
+    @BeforeEach
+    public void beforeEach() {
+        this.user =  new User(1, "losev@danil-m.ru", "Ceeler", "Данил", LocalDate.of(1999, 11, 13));
+    }
+
+    @Test
+    public void shouldHaveZeroViolations() {
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertEquals(0, violations.size());
+    }
+
+    @Test
+    public void shouldHaveOneViolationOnEmptyMail() {
+        user.setEmail("");
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertEquals(1,  violations.size());
+    }
+
+    @Test
+    public void shouldHaveOneViolationOnWrongMail() {
+        user.setEmail("ya.ru");
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertEquals(1,  violations.size());
+    }
+
+    @Test
+    public void shouldHaveTwoViolationOnEmptyLogin() {
+        user.setLogin("");
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertEquals(2,  violations.size());
+    }
+
+    @Test
+    public void shouldHaveOneViolationOnLoginWithSpace() {
+        user.setLogin("My login");
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertEquals(1,  violations.size());
+    }
+
+    @Test
+    public void shouldUseLoginOnEmptyName() {
+        User user1 = new User("losev@danil-m.ru", "Ceeler", LocalDate.of(1999, 11, 13));
+        assertEquals("Ceeler",  user1.getName());
+    }
+
+    @Test
+    public void shouldHaveOneViolationOnFutureBirthday() {
+        user.setBirthday(LocalDate.of(2024,11,13));
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertEquals(1,  violations.size());
+    }
+
+    @Test
+    public void shouldHaveTreeViolationOnFutureBirthdayAndWrongLoginAndEmptyMail() {
+        user.setEmail("");
+        user.setLogin("My login");
+        user.setBirthday(LocalDate.of(2024,11,13));
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertEquals(3,  violations.size());
+    }
+
+}
