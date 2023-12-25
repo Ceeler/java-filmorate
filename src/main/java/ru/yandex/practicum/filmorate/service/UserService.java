8bd1fdb9daac9ead5c546b3ea32ff0618840e8c2
Danil Losev <u4iha.danil@yandex.ru>
2023-12-25 21:56:53 +0300

    feet: добавлены методы для работы с друзьями

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -7,6 +7,8 @@ import ru.yandex.practicum.filmorate.model.entity.User;
 import ru.yandex.practicum.filmorate.model.storage.user.UserStorage;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -55,5 +57,23 @@ public class UserService {
         friend.removeFriend(user);
         userStorage.update(user);
         userStorage.update(friend);
+    }
+
+    public Set<User> getUserFriends(Long id) {
+        User user = userStorage.get(id).orElseThrow(() -> new IllegalArgumentException());
+        return user.getFriends();
+    }
+
+    public Set<User> getUserCommonFriend(Long id, Long otherId) {
+        final Set<User> userFriends = userStorage.get(id)
+                .orElseThrow(() -> new IllegalArgumentException())
+                .getFriends();
+        final Set<User> otherFriends = userStorage.get(otherId)
+                .orElseThrow(() -> new IllegalArgumentException())
+                .getFriends();
+        Set<User> commonFriends = userFriends.stream()
+                .filter(friend -> otherFriends.contains(friend))
+                .collect(Collectors.toSet());
+        return commonFriends;
     }
 }
