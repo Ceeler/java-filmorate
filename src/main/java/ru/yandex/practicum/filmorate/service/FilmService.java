44720c8c5a8aa1377e1f3d6a4e27757ddf9b3688
Danil Losev <u4iha.danil@yandex.ru>
2024-01-11 23:14:28 +0300

    feet: Сервисы переделаны под новые классы

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.entity.Film;
+import ru.yandex.practicum.filmorate.model.storage.film.FilmStorage;
 
 import javax.validation.ConstraintViolationException;
 import java.time.LocalDate;
@@ -10,49 +12,37 @@ import java.util.*;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class FilmService {
 
     private static final LocalDate MIN_FILM_DATE = LocalDate.of(1895, 12, 28);
 
-    private final Map<Long, Film> films = new HashMap<>();
-
-    private long filmSequence = 1;
-
+    private final FilmStorage filmStorage;
 
     public Film getFilmById(long id) {
-        Film film = films.get(id);
-        if (film == null) {
-            throw new IllegalArgumentException();
-        }
+        Film film = filmStorage.get(id).orElseThrow(() -> new IllegalArgumentException());
         return film;
     }
 
     public List<Film> getFilms() {
-        return new ArrayList<>(films.values());
+        return filmStorage.getAll();
     }
 
     public Film addFilm(Film film) {
         log.info("addFilm film={}", film);
-        final long id = filmSequence++;
         if (MIN_FILM_DATE.isAfter(film.getReleaseDate())) {
             throw new ConstraintViolationException("Фильм не может быть создан раньше 28 декабря 1895 года", null);
         }
-        film.setId(id);
-        films.put(id, film);
-        return film;
+        return filmStorage.save(film);
     }
 
     public Film updateFilm(Film film) {
         log.info("updateFilm film={}", film);
-        final long id = film.getId();
         if (MIN_FILM_DATE.isAfter(film.getReleaseDate())) {
             throw new ConstraintViolationException("Фильм не может быть создан раньше 28 декабря 1895 года", null);
         }
-        if (!films.containsKey(id)) {
-            throw new IllegalArgumentException();
-        }
-        films.put(id, film);
-        return film;
+        Film response = filmStorage.update(film);
+        return response;
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,52 +1,59 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.entity.User;
+import ru.yandex.practicum.filmorate.model.storage.user.UserStorage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class UserService {
 
-    private final Map<Long, User> users = new HashMap<>();
-    private long userSequence = 1;
+    private final UserStorage userStorage;
 
     public User getUserById(long id) {
-        User user = users.get(id);
-        if (user == null) {
-            throw new IllegalArgumentException();
-        }
-
+        User user = userStorage.get(id).orElseThrow(() -> new IllegalArgumentException());
         return user;
     }
 
     public List<User> getUsers() {
-        return new ArrayList<>(users.values());
+        return userStorage.getAll();
     }
 
     public User addUser(User user) {
         log.info("addUser user={}", user);
-        final long id = userSequence++;
-        user.setId(id);
         if (user.getName() == null) {
             user.setName(user.getLogin());
         }
-        users.put(id, user);
+        userStorage.save(user);
         return user;
     }
 
     public User updateUser(User user) {
         log.info("updateUser user={}", user);
-        final long id = user.getId();
-        if (!users.containsKey(id)) {
-            throw new IllegalArgumentException();
-        }
-        users.put(id, user);
+        userStorage.update(user);
         return user;
+    }
+
+    public void addFriend(Long id, Long friendId) {
+        User user = userStorage.get(id).orElseThrow(() -> new IllegalArgumentException());
+        User friend = userStorage.get(friendId).orElseThrow(() -> new IllegalArgumentException());
+        user.addFriend(friend);
+        friend.addFriend(user);
+        userStorage.update(user);
+        userStorage.update(friend);
+    }
+
+    public void removeFriend(Long id, Long friendId) {
+        User user = userStorage.get(id).orElseThrow(() -> new IllegalArgumentException());
+        User friend = userStorage.get(friendId).orElseThrow(() -> new IllegalArgumentException());
+        user.removeFriend(friend);
+        friend.removeFriend(user);
+        userStorage.update(user);
+        userStorage.update(friend);
     }
 }
