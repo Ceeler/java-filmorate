7209479720b4b0d14dc0dda76b18cfc99e783cbf
Danil Losev <u4iha.danil@yandex.ru>
2024-01-15 21:56:35 +0300

    refactor: переделаны ответы ошибок

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorControllerAdvice.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorControllerAdvice.java
@@ -2,9 +2,10 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import ru.yandex.practicum.filmorate.model.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.response.Message;
 
 import javax.validation.ConstraintViolationException;
@@ -14,23 +15,32 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
-public class ConstraintControllerAdvice {
+public class ErrorControllerAdvice {
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List<Message>> onConstraintException(ConstraintViolationException e) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<Message> onConstraintException(ConstraintViolationException e) {
         log.warn("Ошибка валидации", e);
         if (e.getConstraintViolations() != null) {
             List<Message> response = e.getConstraintViolations().stream()
                     .map(v -> new Message(v.getMessage()))
                     .collect(Collectors.toList());
-            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+            return response;
         }
-        return new ResponseEntity<>(Collections.singletonList(new Message(e.getMessage())), HttpStatus.BAD_REQUEST);
+        return Collections.singletonList(new Message(e.getMessage()));
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Message> onIllegalArgumentException(IllegalArgumentException e) {
-        log.warn("Ошибка в аргументах запроса", e);
-        return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Message onNotFoundException(NotFoundException e) {
+        log.warn("404 {}", e.getMessage());
+        return new Message(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Message handleThrowable(final Throwable e) {
+        log.warn("500 {}", e.getMessage());
+        return new Message(e.getMessage());
     }
 }
